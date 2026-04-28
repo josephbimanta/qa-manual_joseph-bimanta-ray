577c117c461eea38054d77b8aebe05d0bd5710c7
josephbimanta <joseph.bimanta@gmail.com>
2026-04-28 14:33:34 +0700

    update script for login faield invalid password

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('login success', async ({ page }) => {
+test('TC-01 - Login Success_Valid Credentials', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
   await page.fill('#user-name', 'standard_user');
@@ -8,4 +8,16 @@ test('login success', async ({ page }) => {
   await page.click('#login-button');
 
   await expect(page).toHaveURL(/inventory/);
+});
+
+test('TC-LOG-02 - Login Failed_Invalid password', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'pastieror');
+  await page.click('#login-button');
+
+  await expect(page.locator('[data-test="error"]')).toHaveText(
+    'Epic sadface: Username and password do not match any user in this service'
+  );
 });
